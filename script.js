b373c1a3084881ff1b70979319a72a49848dfef1
Nick <Nick_II2000@mail.ru>
2022-08-08 12:40:02 +0600

    5-th commit

--- script.js
+++ script.js
@@ -8,4 +8,8 @@ $(document).ready(function () {
         // $('.list-item').hover(function () {
         $(this).toggleClass('active');
     });
+
+    $('.list-item:eq(2)').on('click', function () {
+        $('.image:even').fadeToggle('slow');
+    });
 });
